eb9757d075826a986ec692c0b2260fc90923d740
Federico <federicogonz2016@gmail.com>
2021-12-16 16:07:14 -0300

    Creando programa de control de stock con Php 7, Mysql y Bootstrap 5

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include 'template/header.php' ?>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-7">
+          <div class="card">
+            <div class="card-header">
+              Lista de Productos
+            </div>
+            <div class="p-4">
+                  <!-- PARA QUE TODOS LOS ELEMENTOS DENTRO DE LA TABLE ESTEN ALINEADOS-->
+                  <table class="table align-middle">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Descripcion</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Opciones</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td scope="row"></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td scope="row"></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                    </tbody>
+                  </table>
+            </div>
+          </div>
+      </div>
+      <div class="col-md-4 bg-primary">
+          DOS
+      </div>
+    </div>
+</div>
+
+
+<?php include 'template/footer.php' ?> 
---
+++ template/footer.php
@@ -0,0 +1,16 @@
+<!-- CLASE CONTAINER FLUID PARA QUE EL CONTAINER ABARQUE TODO EL ANCHO DE LA PAGINA-->
+  <!-- CLASE FIXED BOTTOM PARA PEGAR LA SECCION O ELEMENTO AL FONDO DE LA PAGINA-->
+  <footer class="container-fluid bg-dark fixed-bottom">
+      <div class="row">
+          <!-- la propiedad py-3 de bs5 da padding del anchor que se indique-->
+          <div class="col-md text-light text-center py-3">
+              Desarrollado por Federico Gonzalez
+          </div>
+      </div>
+  </footer>
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
